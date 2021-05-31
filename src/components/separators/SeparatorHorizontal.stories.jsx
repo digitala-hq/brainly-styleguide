1d13fb1d67a99524e86d3b0476b1da64104b9df4
coderitual <skowronkow@gmail.com>
2021-05-31 18:00:59 +0200

    Add separator horizontal

---
+++ src/components/separators/SeparatorHorizontal.stories.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import SeparatorHorizontal from './SeparatorHorizontal';
+
+export default {
+  title: 'Components/SeparatorHorizontal',
+  parameters: {
+    component: SeparatorHorizontal,
+  },
+};
+
+export const Default = args => <SeparatorHorizontal {...args} />;
